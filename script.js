81183944fd2ee2b6e303237a057121aa5f8ea30a
OmobolajiDurojaiye <durojaiyeomobolaji93@gmail.com>
2024-06-16 21:05:58 +0100

    word counter app

---
+++ script.js
@@ -0,0 +1,25 @@
+const userInput = document.querySelector(".userInput");
+const wordCountDisplay = document.querySelector(".wordCountDisplay");
+const theme = document.querySelector(".theme");
+
+const countWord = (text) => {
+  const words = text
+    .trim()
+    .split(/\s+/)
+    .filter((word) => word.length > 0);
+  return words.length;
+};
+
+userInput.addEventListener("input", () => {
+  const text = userInput.value;
+  const wordCount = countWord(text);
+  wordCountDisplay.textContent = wordCount + " ";
+});
+
+theme.addEventListener("click", function () {
+  if (document.body.style.background === "whitesmoke") {
+    document.body.style.background = "#333";
+  } else {
+    document.body.style.background = "whitesmoke";
+  }
+});
